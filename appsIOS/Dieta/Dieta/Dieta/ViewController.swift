533334761cb97a9ad4706d081e4774086db54840
Maciej <maciek.golianek@gmail.com>
2019-05-27 14:42:58 +0200

    Add apps in progres

---
+++ appsIOS/Dieta/Dieta/Dieta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dieta
+//
+//  Created by Maciej Golianek on 26/05/2019.
+//  Copyright © 2019 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ first app/HitMe/HitMe/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HitMe
+//
+//  Created by Maciej Golianek on 24/05/2019.
+//  Copyright © 2019 Maciej Golianek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert() {
+        print("Hello")
+    }
+    
+}
+
